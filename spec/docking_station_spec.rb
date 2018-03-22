3e27f87d28e25993c383e25701b29948310641bd
Mona Al-Haery <monadrums@gmail.com>
2018-03-22 20:46:06 +0000

    challenge 11 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,12 +12,31 @@ describe DockingStation.new do
   end
 
   it "gets a bike when passed release_bike" do
-   expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
+   expect(subject.release_bike).to be_an_instance_of(Bike)
   end
 
   it "returns true if bike is working" do
     expect(DockingStation.new.release_bike.working?).to be(true).or be(false)
   end
+
+  it "expects to respond to dock" do
+    is_expected.to respond_to (:dock)
+  end
+
+  it "accepts a bike when docking" do
+    is_expected.to respond_to(:dock).with(1).argument
+  end
+
+  bike = Bike.new
+  it "returns a bike when docking" do
+    expect(DockingStation.new.dock(bike)).to eq bike
+  end
+
+  it "user wants to check if bike has been docked" do
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
 
 # Add a test to your DockingStation specification that
